f35e95231b287ae6638217083d51c10d8f440642
oderagowski <oderagowski@gmail.com>
2022-12-21 10:44:21 -0700

    Basic JavaScript assignment #1

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,30 @@
+var A = "Welcome to my website! ";
+document.write(A); // string assigned to var 'A' will appear on the webpage
+
+var B = "This is an alert message";
+window.alert(B); //creates a pop-up alert with the value that is assigned to the variable 'B'
+
+var C = "And Santa said,\"Merry Christmas!\" ";
+document.write(C); // "escaping"" \" is added so the quotes will be ignored by the computer and will remain part of the string
+
+
+document.write("Jingle Bells, Jingle Bells " + "Jingle all the way "); //concatenating strings
+
+var D = "Santa " + "Clause ";
+document.write(D); //concatenating strings
+
+
+var Reindeer = "12", reindeer1 = "Dasher", reindeer2 = "Prancer", reindeer3 = "Vixen", reindeer4 = "Comet", reindeer5 = "Cupid", reindeer6 = "Donner", reindeer7 = "Blitzen", reindeer8 = "Rudolph";
+
+document.write(reindeer8); // the value assigned to the variable "reindeer8" will appear on the webpage
+
+10 + 10; // an expression
+
+
+// ***Submission Assignment #1
+
+var Sent1 = "This is the beginning of the string", Sent2 = " and this is the end of the string"; 
+
+document.write(Sent1 + Sent2); //concatenating strings
+
+
